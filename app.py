3f0e87a2a1f25280dd8d9296fd98b2f3f9d48ab9
lukehayes <lukepurplehayes@gmail.com>
2021-01-21 17:53:59 +0000

    Added correct name generation for downloaded images

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib3
 import requests
-import os
-import sys
+import os, sys, re
 
 # response = requests.get("http://imgsrc.ru/wizarden/68739106.html?pwd=&per_page=12")
 
@@ -18,28 +17,25 @@ except IndexError as e:
     sys.exit()
 
 images = []
+filenames = []
 
 for img in soup.find_all('img', {'data-src': True}):
     images.append(img.get('data-src').lstrip("//"))
 
+pattern = "imgsrc.ru_[0-9A-Za-z]+\.jpg"
 
-counter = 0
-print(len(images))
+for url in images:
+    match = re.search(pattern, url)
+    filenames.append(match.group(0))
 
-for i in images:
-    print(i)
-    r = http.request("GET",i)
-    filename = "image-" + str(counter) + ".jpg"
-    file = open(filename, "wb")
+for i in range(0, len(images)):
+    r = http.request("GET", images[i])
+    file = open(filenames[i], "wb")
+    print("Writing " + filenames[i])
     file.write(r.data)
     file.close()
 
-    counter = counter + 1
-
-if(counter == len(images)):
-    os.system("rm -r img")
-    print("Downloaded Images - Creating Directory...")
-    os.mkdir("img")
-    os.system("mv *.jpg img")
-    print("Completed.")
+print("Downloaded Images - Creating Directory...")
+os.system("mv *.jpg img")
+print("Completed.")
 
